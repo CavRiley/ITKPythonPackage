a7c854b5fe4706ae7744233fcac7cb35568d64f0
Hans Johnson <hans-johnson@uiowa.edu>
2025-12-29 08:35:05 -0600

    BUG: Fix pathing vs string issue.

--- scripts/linux_build_python_instance.py
+++ scripts/linux_build_python_instance.py
@@ -264,9 +264,9 @@ class LinuxBuildPythonInstance(BuildPythonInstanceBase):
         """Resolve virtualenv tool paths.
         platform_env may be a name under IPP_SOURCE_DIR/venvs or an absolute/relative path to a venv.
         """
-        primary_python_base_dir = self.package_env_config[
-            "PYTHON_EXECUTABLE"
-        ].parent.parent
+        primary_python_base_dir = Path(
+            self.package_env_config["PYTHON_EXECUTABLE"]
+        ).parent.parent
         # if True:
         #     self._pip_uninstall_itk_wildcard(self.package_env_config["PYTHON_EXECUTABLE"])
         (
