80bb259eae656571ef5223a94e5dbc770c54f89b
Cavan Riley <cavan-riley@uiowa.edu>
2026-03-03 22:43:56 -0600

    wip: initial windows powershell script added

--- scripts/windows-download-cache-and-build-module-wheels.ps1
+++ scripts/windows-download-cache-and-build-module-wheels.ps1
@@ -1,133 +1,185 @@
 ########################################################################
 # Pull build dependencies and build an ITK external module.
 #
-# This script must be run in an x64 Developer Powershell.
+# This script must be run in an x64 Developer PowerShell.
 # See https://learn.microsoft.com/en-us/visualstudio/ide/reference/command-prompt-powershell?view=vs-2022#developer-powershell
 #
 # -----------------------------------------------------------------------
-# Positional parameters:
-# - 0th parameter or -python_version_minor option: Python minor version.
+# Positional parameters / named options:
+#
+# -python_version_minor  Python minor version (required).
 #     For instance, for Python 3.11:
-#     > windows-download-cache-and-build-module-wheels.ps1 11
-#     or equivalently:
 #     > windows-download-cache-and-build-module-wheels.ps1 -python_version_minor 11
+#     or positionally:
+#     > windows-download-cache-and-build-module-wheels.ps1 11
 #
-# - 1st parameter or -setup_options: pyproject.toml options.
-#     For instance, for Python 3.11, excluding nvcuda.dll during packaging:
-#     > windows-download-cache-and-build-module-wheels.ps1 11 "--exclude-libs nvcuda.dll"
-#     or equivalently:
-#     > windows-download-cache-and-build-module-wheels.ps1 -python_version_minor 11 -setup_options "--exclude-libs nvcuda.dll"
+# -setup_options         pyproject.toml options forwarded to the build script.
+#     For instance, to exclude a library during packaging:
+#     > ... -setup_options "--exclude-libs nvcuda.dll"
 #
-# - 2nd parameter or -cmake_options: CMake options passed to pyproject.tom for project configuration.
-#     For instance, for Python 3.11, excluding nvcuda.dll during packaging
-#     and setting RTK_USE_CUDA ON during configuration:
-#     > windows-download-cache-and-build-module-wheels.ps1 11 "--exclude-libs nvcuda.dll" "-DRTK_USE_CUDA:BOOL=ON"
-#     or equivalently:
-#     > windows-download-cache-and-build-module-wheels.ps1 -python_version_minor 11 -setup_options "--exclude-libs nvcuda.dll" -cmake-options "-DRTK_USE_CUDA:BOOL=ON"
-#
+# -cmake_options         CMake options passed to pyproject.toml for project configuration.
+#     For instance:
+#     > ... -cmake_options "-DRTK_USE_CUDA:BOOL=ON"
 #
 # -----------------------------------------------------------------------
 # Environment variables used in this script:
 #
-# `$env:ITK_PACKAGE_VERSION`: Tag for ITKPythonBuilds build archive to use
+# `$env:ITK_PACKAGE_VERSION`
+#     Tag for the ITKPythonBuilds archive to download/use. Required.
 #
-# `$env:ITKPYTHONPACKAGE_TAG`: Tag for ITKPythonPackage build scripts to use.
-#     If ITKPYTHONPACKAGE_TAG is empty then the default scripts distributed
-#     with the ITKPythonBuilds archive will be used.
+# `$env:ITKPYTHONPACKAGE_TAG`
+#     Tag for ITKPythonPackage build scripts to use.
+#     If empty, the scripts bundled in the archive will be used.
 #
-# `$env:ITKPYTHONPACKAGE_ORG`: Github organization or user to use for ITKPythonPackage
-#     build script source. Default is InsightSoftwareConsortium.
+# `$env:ITKPYTHONPACKAGE_ORG`
+#     GitHub organization/user for ITKPythonPackage. Default: InsightSoftwareConsortium.
 #     Ignored if ITKPYTHONPACKAGE_TAG is empty.
 #
-# `$env:ITK_MODULE_PREQ`: Delimited list of ITK module dependencies to build before building the target module.
-#   Format is `<org_name>/<module_name>@<module_tag>:<org_name>/<module_name>@<module_tag>:...`.
-#   For instance, `export ITK_MODULE_PREQ=InsightSoftwareConsortium/ITKMeshToPolyData@v0.10.0`
+# `$env:ITK_MODULE_PREQ`
+#     Colon-delimited list of ITK module dependencies.
+#     Format: `<org>/<module>@<tag>:<org>/<module>@<tag>:...`
+#     Example: `InsightSoftwareConsortium/ITKMeshToPolyData@v0.10.0`
+#     Passed directly to build_wheels.py via --itk-module-deps; dependency
+#     building is handled inside that script, not here.
+#
+# `$env:MODULE_SRC_DIRECTORY`
+#     Path to the ITK external module source. Defaults to the directory
+#     containing this script.
 #
 ########################################################################
 param (
   [int]$python_version_minor,
-  [string]$setup_options,
-  [string]$cmake_options
+  [string]$setup_options = "",
+  [string]$cmake_options = ""
 )
+
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+
+# Resolve module source directory
+$MODULE_SRC_DIRECTORY = if ($env:MODULE_SRC_DIRECTORY) {
+  $env:MODULE_SRC_DIRECTORY
+} else {
+  $PSScriptRoot
+}
+echo "MODULE_SRC_DIRECTORY: $MODULE_SRC_DIRECTORY"
+
+# Validate required inputs
+if (-not $python_version_minor) {
+  Write-Error "ERROR: -python_version_minor is required. Example: -python_version_minor 11"
+  exit 1
+}
+if (-not $env:ITK_PACKAGE_VERSION) {
+  Write-Error "ERROR: `$env:ITK_PACKAGE_VERSION must be set before running this script."
+  exit 1
+}
 
 $pythonArch = "64"
 $pythonVersion = "3.$python_version_minor"
-echo "Pulling Python $pythonVersion-x$pythonArch"
-iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/scikit-build/scikit-ci-addons/master/windows/install-python.ps1'))
+$ITK_PACKAGE_VERSION = $env:ITK_PACKAGE_VERSION
 
-if (-not $env:ITK_PACKAGE_VERSION) { $env:ITK_PACKAGE_VERSION = 'v5.4.0' }
-echo "Fetching build archive $env:ITK_PACKAGE_VERSION"
-if (Test-Path C:\P) {
-  Remove-Item -Recurse -Force C:\P
-}
-if (-not (Test-Path ITKPythonBuilds-windows.zip)) {
-  Invoke-WebRequest -Uri "https://github.com/InsightSoftwareConsortium/ITKPythonBuilds/releases/download/$env:ITK_PACKAGE_VERSION/ITKPythonBuilds-windows.zip" -OutFile "ITKPythonBuilds-windows.zip"
-}
-7z x ITKPythonBuilds-windows.zip -oC:\P -aoa -r
+$ITKPYTHONPACKAGE_ORG = if ($env:ITKPYTHONPACKAGE_ORG) { $env:ITKPYTHONPACKAGE_ORG } else { "InsightSoftwareConsortium" }
+$ITKPYTHONPACKAGE_TAG = $env:ITKPYTHONPACKAGE_TAG   # may be empty
 
-# Optional: Update ITKPythonPackage build scripts
-if ($env:ITKPYTHONPACKAGE_TAG) {
-  if(-not $env:ITKPYTHONPACKAGE_ORG) {
-    $env:ITKPYTHONPACKAGE_ORG="InsightSoftwareConsortium"
+$DASHBOARD_BUILD_DIRECTORY = "C:\P"
+
+echo "Python version : $pythonVersion-x$pythonArch"
+echo "ITK_PACKAGE_VERSION: $ITK_PACKAGE_VERSION"
+
+# Install Python if not already present
+$pythonExe = "C:\Python$pythonVersion-x$pythonArch\python.exe"
+if (-not (Test-Path $pythonExe)) {
+  echo "Installing Python $pythonVersion-x$pythonArch ..."
+  iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/scikit-build/scikit-ci-addons/master/windows/install-python.ps1'))
+} else {
+  echo "Python already installed at $pythonExe"
+}
+
+# Download ITKPythonBuilds archive (skip if already present)
+$zipName           = "ITKPythonBuilds-windows.zip"
+$zipDownloadUrl    = "https://github.com/InsightSoftwareConsortium/ITKPythonBuilds/releases/download/$ITK_PACKAGE_VERSION/$zipName"
+# Use a version-stamped local filename so different versions coexist
+$localZipName      = "ITKPythonBuilds-windows_${ITK_PACKAGE_VERSION}.zip"
+
+if (Test-Path $localZipName) {
+  echo "Found cached archive: $localZipName -- skipping download."
+} else {
+  echo "Downloading $zipDownloadUrl ..."
+  Invoke-WebRequest -Uri $zipDownloadUrl -OutFile $localZipName
+}
+
+# Unpack archive
+if (Test-Path $DASHBOARD_BUILD_DIRECTORY) {
+  echo "Removing existing build directory: $DASHBOARD_BUILD_DIRECTORY"
+  Remove-Item -Recurse -Force $DASHBOARD_BUILD_DIRECTORY
+}
+echo "Extracting archive to $DASHBOARD_BUILD_DIRECTORY ..."
+7z x $localZipName -o"$DASHBOARD_BUILD_DIRECTORY" -aoa -r
+
+# Optional: update ITKPythonPackage build scripts from a specific tag
+if ($ITKPYTHONPACKAGE_TAG) {
+  echo "Updating build scripts to $ITKPYTHONPACKAGE_ORG/ITKPythonPackage@$ITKPYTHONPACKAGE_TAG"
+
+  $ippTmpDir = "$DASHBOARD_BUILD_DIRECTORY\IPP-tmp"
+  $ippCloneUrl = "https://github.com/$ITKPYTHONPACKAGE_ORG/ITKPythonPackage.git"
+
+  if (-not (Test-Path "$ippTmpDir\.git")) {
+    git clone $ippCloneUrl $ippTmpDir
   }
 
-  echo "Updating build scripts to $env:ITKPYTHONPACKAGE_ORG/ITKPythonPackage@$env:ITKPYTHONPACKAGE_TAG"
-
-  pushd C:\P
-  git clone "https://github.com/$env:ITKPYTHONPACKAGE_ORG/ITKPythonPackage.git" "IPP-tmp"
-  pushd "IPP-tmp"
-  git checkout "$env:ITKPYTHONPACKAGE_TAG"
-  git status
+  pushd $ippTmpDir
+    git checkout $ITKPYTHONPACKAGE_TAG
+    git reset "origin/$ITKPYTHONPACKAGE_TAG" --hard
+    git status
   popd
 
-  Remove-Item -Recurse -Force IPP/scripts/
-  Copy-Item -Recurse IPP-tmp/scripts IPP/
-  Remove-Item -Recurse -Force IPP-tmp/
-  popd
+  # Overlay everything from the cloned repo into IPP
+  Copy-Item -Recurse -Force "$ippTmpDir\*" "$DASHBOARD_BUILD_DIRECTORY\IPP\"
+  Remove-Item -Recurse -Force $ippTmpDir
 }
 
-# Get other build dependencies
-if (-not (Test-Path doxygen-1.8.11.windows.bin.zip)) {
-  Invoke-WebRequest -Uri "https://data.kitware.com/api/v1/file/5c0ad59d8d777f2179dd3e9c/download" -OutFile "doxygen-1.8.11.windows.bin.zip"
-}
-7z x doxygen-1.8.11.windows.bin.zip -oC:\P\doxygen -aoa -r
-if (-not (Test-Path grep-win.zip)) {
+# Fetch other build-time tools
+# Note: doxygen is provided by the pixi environment; no need to download it here.
+if (-not (Test-Path "grep-win.zip")) {
   Invoke-WebRequest -Uri "https://data.kitware.com/api/v1/file/5bbf87ba8d777f06b91f27d6/download/grep-win.zip" -OutFile "grep-win.zip"
 }
-7z x grep-win.zip -oC:\P\grep -aoa -r
-$env:Path += ";C:\P\grep"
+7z x grep-win.zip -o"$DASHBOARD_BUILD_DIRECTORY\grep" -aoa -r
+$env:Path += ";$DASHBOARD_BUILD_DIRECTORY\grep"
 
-# Build ITK module dependencies, if any
-$build_command = "& `"C:\Python$pythonVersion-x$pythonArch\python.exe`" `"C:\P\IPP\scripts\windows_build_module_wheels.py`" --no-cleanup --py-envs `"3$python_version_minor-x64`""
-if ("$setup_options".length -gt 0) {
-  $build_command = "$build_command $setup_options"
+# Assemble the build command
+$buildScript  = "$DASHBOARD_BUILD_DIRECTORY\IPP\scripts\build_wheels.py"
+$buildDirRoot = "$DASHBOARD_BUILD_DIRECTORY\IPP-build"
+$itkSourceDir = "$buildDirRoot\ITK"
+$moduleDepsDir = "$DASHBOARD_BUILD_DIRECTORY\MODULE_DEPENDENCIES"
+$platformEnv  = "win-py3$python_version_minor"
+
+$build_command  = "& `"$pythonExe`" `"$buildScript`""
+$build_command += " --platform-env `"$platformEnv`""
+$build_command += " --module-source-dir `"$MODULE_SRC_DIRECTORY`""
+$build_command += " --module-dependencies-root-dir `"$moduleDepsDir`""
+$build_command += " --itk-module-deps `"$env:ITK_MODULE_PREQ`""
+$build_command += " --no-build-itk-tarball-cache"
+$build_command += " --build-dir-root `"$buildDirRoot`""
+$build_command += " --manylinux-version ``"
+$build_command += " --itk-git-tag `"$ITK_PACKAGE_VERSION`""
+$build_command += " --itk-source-dir `"$itkSourceDir`""
+$build_command += " --itk-package-version `"$ITK_PACKAGE_VERSION`""
+$build_command += " --itk-pythonpackage-org `"$ITKPYTHONPACKAGE_ORG`""
+$build_command += " --itk-pythonpackage-tag `"$ITKPYTHONPACKAGE_TAG`""
+$build_command += " --no-use-ccache"
+$build_command += " --skip-itk-build"
+$build_command += " --skip-itk-wheel-build"
+
+if ($setup_options.Length -gt 0) {
+  $build_command += " $setup_options"
 }
-if("$cmake_options".length -gt 0) {
-  $build_command = "$build_command -- $cmake_options"
+if ($cmake_options.Length -gt 0) {
+  $build_command += " -- $cmake_options"
 }
-echo $build_command
+echo "Build command: $build_command"
 
-echo "ITK_MODULE_PREQ: $env:ITK_MODULE_PREQ $ITK_MODULE_PREQ"
-if ($env:ITK_MODULE_PREQ) {
-  $MODULES_LIST = $env:ITK_MODULE_PREQ.split(":")
-  foreach($MODULE_INFO in $MODULES_LIST) {
-    $MODULE_ORG = $MODULE_INFO.split("/")[0]
-    $MODULE_NAME = $MODULE_INFO.split("@")[0].split("/")[1]
-    $MODULE_TAG = $MODULE_INFO.split("@")[1]
-
-    $MODULE_UPSTREAM = "https://github.com/$MODULE_ORG/$MODULE_NAME.git"
-    echo "Cloning from $MODULE_UPSTREAM"
-    git clone $MODULE_UPSTREAM
-
-    pushd $MODULE_NAME
-    git checkout $MODULE_TAG
-    echo "Building $MODULE_NAME"
-    iex $build_command
-    popd
-
-    Copy-Item $MODULE_NAME/include/* include/
-  }
-}
-
-# Run build scripts
+# ---------------------------------------------------------------------------
+# Build the target module
+# ---------------------------------------------------------------------------
+echo "Building target module ..."
 iex $build_command
